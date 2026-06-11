b66984789a84ceb7081220ffef0475e76c609840
Ndang11 <royaltyhopsin.com>
2026-06-11 15:30:47 +0100

    fix all linters errors

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-import { CONFIG } from './config.js';
+import { CONFIG } from './config.js'
 
 document.addEventListener('DOMContentLoaded', () => {
-  const apiKey = CONFIG.API_KEY;
+  const apiKey = CONFIG.API_KEY
 
   const getWeatherBtn = document.getElementById('get-weather')
   const resetBtn = document.getElementById('reset-weather')
